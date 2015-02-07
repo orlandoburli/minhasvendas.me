d90774430f0d245fdfe1a950d74eb00c29e27b48
gustavoarruda <gustavosantosdev@gmail.com>
2015-02-07 11:53:20 -0200

    Ajuste da cor no input

--- web/assets/minhasvendas-assets/select2-3.5.2/select2.js
+++ web/assets/minhasvendas-assets/select2-3.5.2/select2.js
@@ -3501,14 +3501,11 @@ the specific language governing permissions and limitations under the Apache Lic
     $.fn.select2.locales['en'] = {
          formatMatches: function (matches) { if (matches === 1) { return "One result is available, press enter to select it."; } return matches + " results are available, use up and down arrow keys to navigate."; },
          formatNoMatches: function () { return "No matches found"; },
-         formatAjaxError: function (jqXHR, textStatus, errorThrown) { return "Loading failed"; },
+         formatAjaxError: function (jqXHR, textStatus, errorThrown) { return "Falha ao carregar"; },
          formatInputTooShort: function (input, min) { var n = min - input.length; return "Please enter " + n + " or more character" + (n == 1 ? "" : "s"); },
          formatInputTooLong: function (input, max) { var n = input.length - max; return "Please delete " + n + " character" + (n == 1 ? "" : "s"); },
          formatSelectionTooBig: function (limit) { return "You can only select " + limit + " item" + (limit == 1 ? "" : "s"); },
-         formatLoadMore: function (pageNumber) { return "Loading more results…"; },
-         formatSearching: function () { return "Searching…"; }
-    };
-
+         formatLoadMore: function (pageNumber) { return "Carregando mais resultados
     $.extend($.fn.select2.defaults, $.fn.select2.locales['en']);
 
     $.fn.select2.ajaxDefaults = {
